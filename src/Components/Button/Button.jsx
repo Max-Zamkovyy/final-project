4ef6998bc7880c7175b255f69ef6d8a1bf600658
Maksym Zamkovyy <gloverik1981@gmail.com>
2023-03-03 09:29:18 +0200

    fix

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -3,7 +3,7 @@ import styles from "./Button.module.scss";
 
 const Button = ({ bannerBtn }) => {
   return (
-    <div className="products_bottom">
+    <div className={styles.products_bottom}>
       <a
         href="/"
         className={classNames(styles.btn, styles.btn_outline_primary)}
